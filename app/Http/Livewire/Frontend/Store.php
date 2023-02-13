ba3549721f68181e1f8e80ce76a589b43b59498c
rajugit560 <102935489+rajugit560@users.noreply.github.com>
2023-02-13 08:48:54 +0800

    css scale fix

--- app/Http/Livewire/Frontend/Store.php
+++ app/Http/Livewire/Frontend/Store.php
@@ -10,28 +10,47 @@ use App\Models\Language;
 class Store extends Component
 {
 
-   public $movies, $genreInputs = [], $languageInputs = [];
+   public $movies, $genreInputs = [], $languageInputs = [], $priceInput, $search;
 
    protected $queryString = [
 
     'genreInputs' => ['except' => '', 'as' => 'genre_id'],
     'languageInputs' => ['except' => '', 'as' => 'language_id'],
-    // 'priceInput' => ['except' => '', 'as' => 'price'],
+    'priceInput' => ['except' => '', 'as' => 'price'],
 
     ];
 
+    public function addCart(int $movie_id){
+
+        dd($movie_id);
+    }
+
 
     public function render()
+
     {
         $this->movies = Movies::when($this->genreInputs, function($q){
             $q->whereIn('genre_id',$this->genreInputs);
+
         })->when($this->languageInputs, function($q){
             $q->whereIn('language_id',$this->languageInputs);
-        })
+
+        })->when($this->priceInput, function($q){
+
+            $q->when($this->priceInput == 'high-to-low', function($q2){
+
+                $q2->orderBy('price', 'DESC');
+
+            })->when($this->priceInput == 'low-to-high', function($q2){
+
+                $q2->orderBy('price', 'ASC');
+            });
+        })->where('name', 'like', '%'.$this->search.'%')
         ->where('status','0')->get();
 
         $genres = Genre::all();
         $languages = Language::all();
         return view('livewire.frontend.store',['movies'=>$this->movies,'genres'=>$genres,'languages'=>$languages]);
+
     }
 }
